e4401137ab55a26e3cd1e0b221d631a40347b0b0
Luis Rodríguez <lfrz.arc@gmail.com>
2021-11-28 22:42:24 -0600

    havent interface

--- logic.js
+++ logic.js
@@ -1,10 +1,22 @@
-let signs = ["Rock", "Paper", "Scissors"];
+let signs = ["rock", "paper", "scissors"];
 let cpu = signs[Math.floor(Math.random() * 3)];
 let user;
 
 const game = (user, cpu) => {
-  switch(true){
-    case(user == "Rock" && cpu == "Scissors"):
-      
+  switch (true){
+    case (user == "rock" && cpu == "scissors"):
+      //YouWin!
+      break
+    case (user == "paper" && cpu == "scissors"):
+      //YouWin!
+      break
+    case (user == "scissors" && cpu == "paper"):
+      //YouWin!
+      break
+    case (user == cpu):
+      //Tie
+      break
+    default:
+      //cpuWin
   }
 }
